cb7fca32e5cb0d72c27d9201d055b12949a0292d
ellywynn <whatisyouandme@mail.ru>
2021-10-27 23:11:42 +0300

    project restructuring, running server

---
+++ cmd/web/routes/index.go
@@ -0,0 +1,12 @@
+package routes
+
+import "net/http"
+
+func IndexPage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello there"))
+}
