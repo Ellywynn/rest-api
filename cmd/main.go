e676aa7e1383f428f828d4852a3909d4bcfcff81
= <=>
2021-11-19 22:34:35 +0300

    added config and viper to work with it

--- cmd/main.go
+++ cmd/main.go
@@ -8,19 +8,30 @@ import (
 	"github.com/ellywynn/rest-api/pkg/handler"
 	"github.com/ellywynn/rest-api/pkg/repository"
 	"github.com/ellywynn/rest-api/pkg/service"
+	"github.com/spf13/viper"
 )
 
 const port = "3000"
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("An erroc occurred while initializing config: %s\n", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	s := new(restapi.Server)
-	if err := s.Run(port, handlers.InitRoutes()); err != nil {
+	if err := s.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("An error occurred while running the server: %s\n", err)
 	}
 
 	fmt.Println("Server running on port " + port)
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
